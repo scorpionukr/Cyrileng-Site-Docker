c67671b262f4cbdf82115c32db750345c731f0d3
Vitaliy <scorpionukr@gmail.com>
2025-04-15 20:09:27 +0300

    cyrileng commit

---
+++ wp-data/themes/mesmerize/header-homepage.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window,document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		 fbq('init', '186776862214473'); 
+		fbq('track', 'PageView');
+	</script>
+	<noscript>
+		 <img height="1" width="1" 
+		src="https://www.facebook.com/tr?id=186776862214473&ev=PageView
+		&noscript=1"/>
+	</noscript>
+	<!-- End Facebook Pixel Code -->
+	<script>
+	function leadCl() {
+			var btn=document.getElementsByClassName('lead-link');
+			//e.preventDefault();
+			fbq('track', 'Lead');
+			document.location.href="https://cyrileng.com/?page_id=109";
+		}
+	</script>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+
+<div id="page-top" class="header-top homepage">
+    <?php mesmerize_print_header_top_bar(); ?>
+    <?php mesmerize_get_navigation(); ?>
+</div>
+
+
+<div id="page" class="site">
+
+    <?php mesmerize_print_hero(); ?>
---
+++ wp-data/themes/mesmerize/template-parts/navigation/navigation-logo-above-menu.php
@@ -0,0 +1,39 @@
+<div class="navigation-bar logo-above-menu <?php mesmerize_header_main_class() ?>" <?php mesmerize_navigation_sticky_attrs() ?>>
+    <div class="navigation-wrapper <?php mesmerize_navigation_wrapper_class() ?>">
+        <div class="row basis-auto between-xs">
+	        <div class="logo_col col-xs col-sm-12 center-sm">
+	            <?php mesmerize_print_logo(); ?>
+	        </div>
+	        <div class="main_menu_col col-xs-fit col-sm">
+	            <?php //mesmerize_print_primary_menu(); ?>
+				<div id="mainmenu_container" class="row"><ul id="main_menu" class="active-line-bottom main-menu dropdown-menu"><li id="menu-item-66" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-66"><a href="https://cyrileng.com">О Cyrileng</a></li>
+<li id="menu-item-1233" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1233"><a href="https://cyrileng.com/#testimonials-1">Отзывы</a></li>
+<li id="menu-item-1232" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1232"><a href="https://cyrileng.com/#c">Начать обучение</a></li>
+<li id="menu-item-1231" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1231"><a href="https://cyrileng.com/#features-3">Цены</a></li>
+<li id="menu-item-1230" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1230"><a href="https://cyrileng.com/#contact-1">Контакты</a></li>
+<li id="menu-item-1013" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-109 current_page_item menu-item-1013">
+	<a href="#" class="lead-link" onclick="leadCl();">Оплата\Регистрация</a>
+</li>
+</ul></div>    <a href="#" data-component="offcanvas" data-target="#offcanvas-wrapper" data-direction="right" data-width="300px" data-push="false">
+        <div class="bubble"></div>
+        <i class="fa fa-bars"></i>
+    </a>
+    <div id="offcanvas-wrapper" class="hide force-hide  offcanvas-right">
+        <div class="offcanvas-top">
+            <div class="logo-holder">
+                <a href="https://cyrileng.com/" class="custom-logo-link" rel="home" itemprop="url"><img width="600" height="200" src="https://cyrileng.com/wp-content/uploads/2018/09/cropped-received_6465477757457711-1.jpeg" class="custom-logo" alt="Basic English online with cyrileng" itemprop="logo" srcset="https://cyrileng.com/wp-content/uploads/2018/09/cropped-received_6465477757457711-1.jpeg 600w, https://cyrileng.com/wp-content/uploads/2018/09/cropped-received_6465477757457711-1-300x100.jpeg 300w" sizes="(max-width: 600px) 100vw, 600px" /></a>            </div>
+        </div>
+        <div id="offcanvas-menu" class="menu-%d0%b2%d0%b5%d1%80%d1%85%d0%bd%d0%b5%d0%b5-%d0%bc%d0%b5%d0%bd%d1%8e-container"><ul id="offcanvas_menu" class="offcanvas_menu"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-66"><a href="https://cyrileng.com">О Cyrileng</a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1233"><a href="https://cyrileng.com/#testimonials-1">Отзывы</a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1232"><a href="https://cyrileng.com/#c">Начать обучение</a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1231"><a href="https://cyrileng.com/#features-3">Цены</a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-home menu-item-1230"><a href="https://cyrileng.com/#contact-1">Контакты</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-109 current_page_item menu-item-1013">
+	<a href="#" class="lead-link" onclick="leadCl();">Оплата\Регистрация</a>
+</li>
+</ul></div>
+            </div>
+	        </div>
+	    </div>
+    </div>
+</div>
